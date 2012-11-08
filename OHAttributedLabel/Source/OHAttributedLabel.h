4ea55ec5c32a4a17d855a108dfe402f2d45773a7
ohalligon <olivier.halligon@niji.fr>
2012-11-08 16:45:15 +0100

    CF_OPTIONS -> NS_OPTIONS

--- OHAttributedLabel/Source/OHAttributedLabel.h
+++ OHAttributedLabel/Source/OHAttributedLabel.h
@@ -54,11 +54,16 @@ __attribute__((deprecated("You should use 'setTextAlignment:lineBreakMode:' on y
 __attribute__((unavailable("Since iOS6 SDK, you have to use 'setTextAlignment:lineBreakMode:' on your NSAttributedString instead.")));
 #endif
 
+#ifndef NS_OPTIONS
+// For older compilers compatibility. But you should really update your Xcode and LLVM.
+#define NS_OPTIONS(_type, _name) _type _name; enum _name
+#endif
+
 //! This flags are designed to be used with the linkUnderlineStyle property and the -attributedLabel:colorForLink:underlineStyle: delegate method
 //! - If you bitwise-OR the linkUnderlineStyle with the kOHBoldStyleTraitSetBold constant, the bold attribute will be added to links in text
 //! - If you bitwise-OR the linkUnderlineStyle with the kOHBoldStyleTraitUnSetBold constant, the bold attribute will be removed from links in text
 //! - If you don't use these constants, the bold attribute of links will not be altered (so it will be bold if link was already in a bold text portion, non-bold if not)
-typedef CF_OPTIONS(int32_t, OHBoldStyleTrait)  {
+typedef NS_OPTIONS(int32_t, OHBoldStyleTrait) {
     kOHBoldStyleTraitMask       = 0x030000,
     kOHBoldStyleTraitSetBold    = 0x030000,
     kOHBoldStyleTraitUnSetBold  = 0x020000,
