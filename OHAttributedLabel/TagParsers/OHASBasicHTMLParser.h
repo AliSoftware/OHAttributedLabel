63f7b05c9f80b57d3299fd7364802910154606a7
Olivier Halligon <olivier@halligon.net>
2012-11-18 20:54:01 +0100

    Updated headers documentation for Markup Parsers

--- OHAttributedLabel/TagParsers/OHASBasicHTMLParser.h
+++ OHAttributedLabel/TagParsers/OHASBasicHTMLParser.h
@@ -10,14 +10,21 @@
 
 /*!
  * Supported tags:
- *  "<b>bold text</b>"
- *  "<u>underline text</u>"
- *  "<font name='fontname' size='size'>some text</font>"
+ *
+ *  "<b>bold text</b>" to write some text in bold
+ *
+ *  "<u>underline text</u>" to write some text underlined
+ *
+ *  "<font name='fontname' size='size'>some text</font>" to change font and size of some text
  *      note that you have to specify both "name" and "size" attributes and in that exact order
- *  "<font color='color'>some text</font>"
+ *
+ *  "<font color='color'>some text</font>" to change text color
  *      where color can be an hexadecimal color of the form "#rgb", "#rgba", "#rrggbb" or "#rrggbbaa"
  *      or a color name like "red" "green" "blue", "purple"…
  *      (the supported names correspond to the [UIColor xxxColor] commodity constructors of the UIColor class)
+ *
+ *  "<a href='link'>some text</a>" to add some links to a text
+ *
  */
 @interface OHASBasicHTMLParser : OHASMarkupParserBase
 
--- OHAttributedLabel/TagParsers/OHASBasicMarkupParser.h
+++ OHAttributedLabel/TagParsers/OHASBasicMarkupParser.h
@@ -10,12 +10,20 @@
 
 /*!
  * Supported tags:
- *  "*bold*"
- *  "_underline_"
- *  "`typewritter font`"
+ *
+ *  "*bold*" to write text in bold
+ *
+ *  "_underline_" to write text underlined
+ *
+ *  "`typewritter font`" to write text in Courier (fixed-width) font
+ *
+ *  "[some text](some URL)" to create a link to "some URL" with displayed text "some text"
+ *
  * Colors:
  *  - use "{color|text}" like in "{#fc0|some purple text}" and "{#00ff00|some green text}"
  *  - It also support named colors like in "{red|some red text}" "{green|some green text}", "{blue|some blue text}"
+ *
+ * 
  */
 @interface OHASBasicMarkupParser : OHASMarkupParserBase
 
